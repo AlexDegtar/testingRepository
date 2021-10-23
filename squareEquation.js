7b3c875023be3fda6e19abac67af8a3bb50aeadf
AlexDegtar <degtaraleksandr89@gmail.com>
2021-10-23 19:39:45 +0300

    rename index.js

---
+++ squareEquation.js
@@ -0,0 +1,17 @@
+const numbers = [1, 0, -4];
+
+let squareEquation = (arr) => {
+    let [a, b, c] = arr;
+    let descreminant = b * b - 4 * a * c;
+
+    if (descreminant < 0) {
+        return `Descreminant is ${descreminant}, no square equation`;
+    } else if (descreminant == 0) {
+        let x = `Descreminant is ${descreminant}, x: ${b / 2 * a}`;
+        return x;
+    } else if (descreminant > 0) {
+        let x1 = `Descreminant is ${descreminant}, x1: ${(b - Math.sqrt(descreminant)) / 2 * a} and x2: ${(b + Math.sqrt(descreminant)) / 2 * a}`
+        return x1;
+    }
+};
+console.log(squareEquation(numbers));
